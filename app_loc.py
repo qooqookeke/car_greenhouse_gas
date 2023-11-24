a0d02237a0af333b0999e27499e1249f202364b3
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2023-11-24 23:04:58 +0900

    add

--- app_loc.py
+++ app_loc.py
@@ -3,22 +3,13 @@ import pandas as pd
 
 def run_loc_app():
     df = pd.read_csv('./data/hospital_data.csv', encoding='euc-kr')
-    df_location = df.iloc[:,[28,29]]
-    df_location = df.rename(columns={'좌표(Y)':'lat','좌표(X)':'lon'})
-    df_location = df_location[['lat','lon']]
-    df_location = df_location.dropna()
 
-    st.dataframe(df_location)
-    st.map(data=df_location, )
-
-
-    df.iloc[:[]]
-    st.map(data=df_location, )
-
-
-    df_search = df.iloc[:,[5,7,10,11,12,28,29]]
-    df_search = df_search.rename(columns={'좌표(Y)':'lat','좌표(X)':'lon'})
-    df_search = df_search[['lat','lon']]
-    df_search = df_search.dropna()
+    st.text_input('병원 검색')
+    df_search = df.iloc[:,[1,10,11,12,28,29]]
+    df_search = df_search.rename(columns={'요양기관명':'병원이름','좌표(Y)':'lat','좌표(X)':'lon'})
+    df_search = df_search[['병원이름','주소','전화번호','병원홈페이지','lat','lon']]
+    df_search = df_search.fillna('-')
+    
     
     st.dataframe(df_search)
+    st.map(data=df_search, )
