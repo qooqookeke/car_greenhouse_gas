7a4d6bdd1e02b0a65ab69d3afb352e8a290afd01
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2023-11-25 22:23:16 +0900

    add

--- app_home.py
+++ app_home.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 def run_home_app():
     st.text('이 앱은 전국의 병원 데이터를 모아둔 곳입니다.')
 
-    img_url = 'https://img.freepik.com/free-vector/people-walking-and-sitting-at-hospital-building-city-clinic-glass-exterior-flat-vector-illustration-for-medical-help-emergency-architecture-healthcare-concept_74855-10130.jpg'
-    st.image=(img_url)
+    img_url ='https://cdn.pixabay.com/photo/2020/08/03/09/43/medical-5459654_1280.png'
+    st.image(img_url)
+    
+    st.subheader('공공데이터')
+    st.link_button('데이터 보러가기','https://opendata.hira.or.kr/op/opc/selectOpenData.do?sno=11925&publDataTpCd=&searchCnd=&searchWrd=%EC%A0%84%EA%B5%AD&pageIndex=1')
+
+    df = pd.read_csv('./data/hospital_data.csv', encoding='euc-kr')
+    df = df.iloc[:,1:]
+    st.dataframe(df)
--- app_info.py
+++ app_info.py
@@ -6,20 +6,28 @@ import seaborn as sb
 import plotly.express as px
 
 def run_info_app():
-    st.header('지역별 병원 현황') 
-
-    st.subheader('공공데이터')
-    st.link_button('데이터 보러가기','https://opendata.hira.or.kr/op/opc/selectOpenData.do?sno=11925&publDataTpCd=&searchCnd=&searchWrd=%EC%A0%84%EA%B5%AD&pageIndex=1')
-
+    
     df = pd.read_csv('./data/hospital_data.csv', encoding='euc-kr')
+    df = df.iloc[:,1:28]
+    st.header('지역별 병원 현황') 
     st.dataframe(df)
     
+    st.subheader('시도별 병원의 갯수')
+    location = df['시도코드명'].value_counts().index
+    plt.rc('font', family='Malgun Gothic')
+    fig1 = plt.figure(figsize=(7,9))
+    sb.countplot(data=df, y='시도코드명', order=location)
+    plt.title('지역별 병원 수')
+    plt.xlabel('병원 수')
+    plt.ylabel('지역')
+    st.pyplot(fig1)
+    
+    st.subheader('지역별 병원수 비율')
     loc1 = df['시도코드명'].value_counts()
     loc1 = loc1.to_frame().reset_index()
-    
-    st.dataframe(loc1)
+    loc1 = loc1.rename(columns={'시도코드명':'시도','count':'갯수'})
         
-    chart1 = px.pie(data_frame=loc1, names='시도코드명', values='count',
-                    title='각 지역별 병원 비율 파이차트')
+    chart1 = px.pie(data_frame=loc1, names='시도', values='갯수',
+                    title='각 지역별 병원수 비율 파이차트')
     chart1.update_traces(textposition='inside', textinfo='percent+label')
     st.plotly_chart(chart1)
