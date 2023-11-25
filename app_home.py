44d20c3bb71e681a7fa947c2c2cfeaf2ee5e3c69
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2023-11-25 22:27:53 +0900

    add

--- app_home.py
+++ app_home.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 
 def run_home_app():
     st.text('이 앱은 전국의 병원 데이터를 모아둔 곳입니다.')
 
-    img_url ='https://cdn.pixabay.com/photo/2020/08/03/09/43/medical-5459654_1280.png'
-    st.image(img_url)
+    img = Image.open('./img/medical-5459654_1280.png')
+    st.image(img)
     
     st.subheader('공공데이터')
     st.link_button('데이터 보러가기','https://opendata.hira.or.kr/op/opc/selectOpenData.do?sno=11925&publDataTpCd=&searchCnd=&searchWrd=%EC%A0%84%EA%B5%AD&pageIndex=1')
