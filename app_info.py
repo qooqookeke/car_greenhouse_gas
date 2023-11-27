527bc7646a290c802e1ac56c37740656e590af04
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2023-11-27 13:37:29 +0900

    add

--- app_info.py
+++ app_info.py
@@ -5,6 +5,12 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 import plotly.express as px
 
+import platform
+from matplotlib import font_manager, rc
+plt.rcParams['axes.unicode_minus'] = False
+if platform.system() == 'Linux':
+    rc('font', family='NanumGothic')
+
 def run_info_app():
     
     df = pd.read_csv('./data/hospital_data.csv', encoding='euc-kr')
@@ -14,7 +20,7 @@ def run_info_app():
     #
     st.subheader('시도별 병원의 갯수')
     location = df['시도코드명'].value_counts().index
-    plt.rc('font', family='Malgun Gothic')
+    # plt.rc('font', family='Malgun Gothic')
     fig1 = plt.figure(figsize=(7,9))
     sb.countplot(data=df, y='시도코드명', order=location)
     plt.title('지역별 병원 수')
