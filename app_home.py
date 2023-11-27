b3e6571c875d60dbafd04b13f8d05bd2c3abc7e7
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2023-11-27 10:04:41 +0900

    add

--- app_home.py
+++ app_home.py
@@ -4,13 +4,16 @@ from PIL import Image
 
 def run_home_app():
     st.text('이 앱은 전국의 병원 데이터를 모아둔 곳입니다.')
-
+    
+    #
     img = Image.open('./img/medical-5459654_1280.png')
     st.image(img)
     
+    ##
     st.subheader('공공데이터')
-    st.link_button('데이터 출처','https://opendata.hira.or.kr/op/opc/selectOpenData.do?sno=11925&publDataTpCd=&searchCnd=&searchWrd=%EC%A0%84%EA%B5%AD&pageIndex=1')
+    st.link_button('데이터 출처 보러가기','https://opendata.hira.or.kr/op/opc/selectOpenData.do?sno=11925&publDataTpCd=&searchCnd=&searchWrd=%EC%A0%84%EA%B5%AD&pageIndex=1')
 
+    ###
     df = pd.read_csv('./data/hospital_data.csv', encoding='euc-kr')
     df = df.iloc[:,1:]
     st.dataframe(df)
--- app_info.py
+++ app_info.py
@@ -10,18 +10,8 @@ def run_info_app():
     df = pd.read_csv('./data/hospital_data.csv', encoding='euc-kr')
     df = df.iloc[:,1:28]
     st.header('지역별 병원 현황')
-    # st.dataframe(df)
-       
-    df.iloc[:,4].unique()
-    loc_name = df.iloc[:,4].unique()
-    selected_list = st.multiselect('지역을 선택하세요', loc_name)
-    if len(selected_list) != 0:
-        selected_data = df[(df['시도코드명'].isin(selected_list))]
-        st.dataframe(selected_data)
-    else:
-        st.text('')
 
-    
+    #
     st.subheader('시도별 병원의 갯수')
     location = df['시도코드명'].value_counts().index
     plt.rc('font', family='Malgun Gothic')
@@ -32,6 +22,7 @@ def run_info_app():
     plt.ylabel('지역')
     st.pyplot(fig1)
     
+    ##
     st.subheader('지역별 병원수 비율')
     loc1 = df['시도코드명'].value_counts()
     loc1 = loc1.to_frame().reset_index()
@@ -41,3 +32,30 @@ def run_info_app():
                     title='각 지역별 병원수 비율 파이차트')
     chart1.update_traces(textposition='inside', textinfo='percent+label')
     st.plotly_chart(chart1)
+
+    ###   
+    df.iloc[:,4].unique()
+    loc_name = df.iloc[:,4].unique()
+    selected_list = st.selectbox('지역을 선택하세요', loc_name)
+    if len(selected_list) != 0:
+        selected_data = df[(df['시도코드명'].isin([selected_list]))]
+        st.dataframe(selected_data)
+        order1 = selected_data['시군구코드명'].value_counts().head(10).index
+        fig2 = plt.figure()
+        sb.countplot(data= df, x='시군구코드명', order=order1)
+        ax = sb.countplot(data=df, x='시군구코드명', order=order1)
+        for p in ax.patches:
+            height = p.get_height()
+            ax.text(x = p.get_x() + p.get_width() / 2, 
+                    y = height + 10, 
+                    s = int(height), 
+                    ha = 'center', 
+                    size = 9)
+        # ax.set_ylim(0, 3000)
+        plt.title('시군구별 병원수 상위 10개')
+        plt.xticks(rotation = 60)
+        plt.xlabel('시군구')
+        plt.ylabel('갯수')
+        st.pyplot(fig2)
+    else:
+        st.text('')
