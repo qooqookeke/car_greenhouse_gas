17627fdd6527a54a974692332157d58f3d09e221
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2023-11-28 12:13:51 +0900

    add

--- app_loc.py
+++ app_loc.py
@@ -5,6 +5,8 @@ import numpy as np
 def run_loc_app():
     df = pd.read_csv('./data/hospital_data.csv', encoding='euc-kr')
 
+    st.subheader('병원 위치 및 대표전화')
+
     df_search = df.iloc[:,[1,10,11,12,28,29]]
     df_search = df_search.rename(columns={'요양기관명':'병원명','좌표(Y)':'lat','좌표(X)':'lon'})
     df_search = df_search[['병원명','주소','전화번호','병원홈페이지','lat','lon']]
