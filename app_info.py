e3a06c762ff3e014686af989b7171521f89447f8
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2023-11-28 22:24:30 +0900

    add

--- app_info.py
+++ app_info.py
@@ -64,6 +64,6 @@ def run_info_app():
 
     doc_count = df['총의사수'].max()
     doc_max = df.loc[df['총의사수'] == df['총의사수'].max(), ]
-    max_name = doc_max['요양기관명']
+    max_name = doc_max['요양기관명'].iloc[0]
     st.text('전국에서 가장 많은 의사가 있는 병원은 {} 이며,'.format(max_name))
     st.text('의사 수는 {} 명 입니다.'.format(doc_count))
